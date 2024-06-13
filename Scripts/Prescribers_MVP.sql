468795b793c25162a75c5419bf6dbaceed2e4a73
Zavello <zechariahavello@gmail.com>
2024-06-12 22:44:18 -0400

    CompleteThroughQ4

--- Scripts/Prescribers_MVP.sql
+++ Scripts/Prescribers_MVP.sql
@@ -1,18 +1,18 @@
 -- 1. 
 --     a. Which prescriber had the highest total number of claims (totaled over all drugs)? Report the npi and the total number of claims.
 
-Select prescriber.npi,
-	prescriber.nppes_provider_first_name As first_name,
-	prescriber.nppes_provider_last_org_name As last_name,
-	Sum(prescription.total_claim_count) As total_claim_count
-From prescriber
-Inner Join prescription
-Using (npi)
-Group By prescriber.npi,
-	prescriber.nppes_provider_first_name,
-	prescriber.nppes_provider_last_org_name
-Order By total_claim_count DESC
-Limit 1;
+-- Select prescriber.npi,
+-- 	prescriber.nppes_provider_first_name As first_name,
+-- 	prescriber.nppes_provider_last_org_name As last_name,
+-- 	Sum(prescription.total_claim_count) As total_claim_count
+-- From prescriber
+-- Inner Join prescription
+-- Using (npi)
+-- Group By prescriber.npi,
+-- 	prescriber.nppes_provider_first_name,
+-- 	prescriber.nppes_provider_last_org_name
+-- Order By total_claim_count DESC
+-- Limit 1;
 
 -- Bruce Pendley, NPI number 1881634483 with 99707 claims
 
@@ -70,26 +70,52 @@ Limit 1;
 -- 3. 
 --     a. Which drug (generic_name) had the highest total drug cost?
 
-Select drug_name, 
-	(total_drug_cost + total_drug_cost_ge65) As total_cost
-From prescription
-Order By total_cost DESC;
+-- Select drug.generic_name, 
+-- 	cast(prescription.total_drug_cost As money) As total_cost
+-- From prescription
+-- Inner Join drug
+-- Using (drug_name)
+-- Order By total_cost DESC;
 
--- Esbriet
+-- Pirfendone
 
 
 --     b. Which drug (generic_name) has the hightest total cost per day? **Bonus: Round your cost per day column to 2 decimal places. Google ROUND to see how this works.**
 
--- Select drug_name, 
--- 	Round((total_drug_cost + total_drug_cost_ge65)/total_day_supply,2) As total_cost_per_day
+-- Select drug.generic_name, 
+-- 	Round(total_drug_cost/total_day_supply,2) As total_cost_per_day
 -- From prescription
+-- Inner Join drug
+-- Using (drug_name)
 -- Order By total_cost_per_day DESC;
 
+-- IMMUN GLOB G(IGG)/GLY/IGA OV50
 
 -- 4. 
 --     a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs. **Hint:** You may want to use a CASE expression for this. See https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-case/ 
 
+-- Select drug_name,
+-- 	Case When opioid_drug_flag = 'Y' Then 'opioid'
+-- 		When antibiotic_drug_flag = 'Y' Then 'antibiotic' End As drug_type
+-- From drug
+-- Where Case When opioid_drug_flag = 'Y' Then 'opioid'
+-- 		When antibiotic_drug_flag = 'Y' Then 'antibiotic' End IS Not Null;
+
 --     b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision.
+
+-- Select Case When opioid_drug_flag = 'Y' Then 'opioid'
+-- 		When antibiotic_drug_flag = 'Y' Then 'antibiotic' End As drug_type,
+-- 	Cast(Sum(total_drug_cost) As money) As total_cost
+-- From drug
+-- Inner Join prescription
+-- 	Using (drug_name)
+-- Where Case When opioid_drug_flag = 'Y' Then 'opioid'
+-- 		When antibiotic_drug_flag = 'Y' Then 'antibiotic' End IS Not Null
+-- Group By Case When opioid_drug_flag = 'Y' Then 'opioid'
+-- 		When antibiotic_drug_flag = 'Y' Then 'antibiotic' End
+-- Order By total_cost DESC;
+
+-- Opioid cost more than antibiotic
 
 -- 5. 
 --     a. How many CBSAs are in Tennessee? **Warning:** The cbsa table contains information for all states, not just Tennessee.
